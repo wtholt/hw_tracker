5e1201711f24139d952f29177dabff7e68e73c7d
William Holt <wtholt27@gmail.com>
2015-03-26 15:14:45 -0400

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module StudentHwTracker
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.initialize_on_precompile = false
+
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
